4ea8b0f3c85e52686d8567f78d830f253586a61b
Danvir Guram <james.tarball@gmail.com>
2017-07-31 20:48:05 +0100

    TG-121: ADDED files

--- scripts/aws/openvpn.sh
+++
@@ -1,9 +0,0 @@
-openvpn
-g**********12
-
-ok all need to do is add vpc subnet IPV4 CIDR
-
-openvpn MENU
-Allow Access To these Networks:
-
-http://sysextra.blogspot.co.uk/2011/01/creating-virtual-private-cluster-with.html
