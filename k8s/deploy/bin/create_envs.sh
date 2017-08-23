22de409c6baeb35ed02d5d2dda8c474489ad3e1b
Danvir Guram <james.tarball@gmail.com>
2017-08-23 16:39:25 +0100

    TG-163: Delete files and only use one mongo db replica cluster (namespace = dev-common)

--- k8s/deploy/bin/create_envs.sh
+++
@@ -1,3 +0,0 @@
-kubectl --namespace=dev-common create -f dev-common/
-kubectl --namespace=master create -f master/
-kubectl --namespace=featuretest create -f featuretest/
--- k8s/deploy/bin/delete_envs.sh
+++
@@ -1,3 +0,0 @@
-kubectl --namespace=master delete -f master/
-kubectl --namespace=featuretest delete -f featuretest/
-kubectl --namespace=dev-common delete -f dev-common/
