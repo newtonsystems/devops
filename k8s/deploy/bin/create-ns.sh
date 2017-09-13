8e8d25e3ebda6ef85ffb207527e7a7c8d527770d
Danvir Guram <james.tarball@gmail.com>
2017-09-13 19:59:59 +0100

    update scripts

--- k8s/deploy/bin/create-ns.sh
+++ k8s/deploy/bin/create-ns.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+#
+# create-ns.sh
+#
+# Creates kubernetes namespaces
+#
+# Make sure you have NEWTON_PATH set 
+#
+if [ -z $NEWTON_PATH ]; then
+    echo "You have not set the environment variable NEWTON_PATH. Please set."
+    echo ""
+    echo "For example:"
+    echo "    export NEWTON_PATH=/Users/danvir/Masterbox/sideprojects/github/newtonsystems/"
+    echo ""
+    exit
+fi
+
+cd $NEWTON_PATH/devops/k8s/deploy
+
 kubectl create -f namespaces/
